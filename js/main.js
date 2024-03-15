1969cf0ceceeca9e593147f001bdeeb27e0a42f1
David Reckner <dreckner@reckner.com>
2024-03-15 15:59:27 -0400

    try nonce

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+<script nonce="g^9fn$Tw%P&2!G">
 function showHide(did) {
 	var s = "block";
 	if (document.getElementById(did).style.display == "block") s = "none";
 	document.getElementById(did).style.display = s;
 }
+</script>
